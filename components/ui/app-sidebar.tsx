65803e16152961bdd1ca02a14f97e2db7f808a08
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2025-03-24 13:39:00 +0800

    fix: use next link in app sidebar

--- components/ui/app-sidebar.tsx
+++ components/ui/app-sidebar.tsx
@@ -10,6 +10,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
+import Link from 'next/link';
 
 const items = [
   {
@@ -35,10 +36,10 @@ export function AppSidebar() {
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                    <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
