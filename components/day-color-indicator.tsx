8b792e4dbe7d017e32d5926fd7dc3bd6195c9911
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2025-03-27 23:01:08 +0800

    fix: holiday selection background color - add cursor pointer to checkboxes

--- components/day-color-indicator.tsx
+++ components/day-color-indicator.tsx
@@ -1,13 +1,13 @@
 export default function DayColorIndicator() {
   return (
     <div className='flex gap-1'>
-      <div className='py-[2px] px-3 bg-red-200 text-red-900 rounded-full'>
+      <div className='py-[2px] px-3 bg-red-300 text-red-900 rounded-full'>
         <p className='text-sm font-medium'>Sunday</p>
       </div>
-      <div className='py-[2px] px-3 bg-blue-200 text-blue-900 rounded-full'>
+      <div className='py-[2px] px-3 bg-blue-300 text-blue-900 rounded-full'>
         <p className='text-sm font-medium'>Holiday</p>
       </div>
-      <div className='py-[2px] px-3 bg-purple-200 text-purple-900 rounded-full'>
+      <div className='py-[2px] px-3 bg-purple-300 text-purple-900 rounded-full'>
         <p className='text-sm font-medium'>Sunday & Holiday</p>
       </div>
     </div>
